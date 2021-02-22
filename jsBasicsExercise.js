ea6ea17af929cc3801e39224ea5e31734970f648
Murphy Lanham <murphylanham@KCCCs-MacBook-Pro.local>
2021-02-22 13:53:54 -0500

    Final Commit

---
+++ jsBasicsExercise.js
@@ -0,0 +1,10 @@
+console.log(7);
+
+// numbers, strings, booleans, null, undefined
+// +  -  *  /  **  %
+console.log(5%2);
+console.log(5%4);
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+console.log("Wanda" + "Vision");
+console.log("Murphy".length + "Lanham".length);
+console.log("Avengers Endgame"[15]);
